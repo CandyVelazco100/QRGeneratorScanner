12e853bbdc556c5c6b13a08443dac72abf1ed973
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-07-06 16:03:34 -0700

    "Updated generator.js: changed imgURL initialization, added input trimming, and implemented QR code download functionality."

--- js/generator.js
+++ js/generator.js
@@ -4,16 +4,16 @@ const qrInput = document.querySelector(".generator-form input");
 const qrImg = document.querySelector(".generator-img img");
 const downloadBtn = document.querySelector(".generator-btn .btn-link");
 
-let imgURL = ' ';
+let imgURL = '';
 
 /**
  * Handles the generate button click event.
 **/
 generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value;
-    if(!qrValue.trim()) return;
+    let qrValue = qrInput.value.trim();
+    if (!qrValue) return;
 
-    generateBtn.innerText = "Generating QR Code... ";
+    generateBtn.innerText = "Generating QR Code...";
 
     imgURL = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
     qrImg.src = imgURL;
@@ -21,7 +21,41 @@ generateBtn.addEventListener("click", () => {
     qrImg.addEventListener("load", () => {
         generatorDiv.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
-    })
-    
-    console.log(imgURL);
-})
+    });
+});
+
+/**
+ * Handles the download button click event.
+ * Downloads the generated QR code image as a PNG file.
+ */
+downloadBtn.addEventListener("click", () => {
+    if (!imgURL) return;
+    fetchImage(imgURL);
+});
+
+/**
+ * Fetches the QR code image from the API and downloads it as a file.
+ */
+function fetchImage(url) {
+    fetch(url)
+        .then(res => res.blob())
+        .then(file => {
+            let file_name = "qr_code";
+            let extension = "png";
+            download(URL.createObjectURL(file), file_name, extension);
+        })
+        .catch(() => imgURL = '');
+}
+
+
+/**
+ * Downloads a file from a temporary URL.
+ */
+function download(tempFile, file_name, extension) {
+    let a = document.createElement('a');
+    a.href = tempFile;
+    a.download = `${file_name}.${extension}`;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+}
